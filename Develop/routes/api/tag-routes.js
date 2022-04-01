352950322777fc162e4611e5e457201664c256af
AmyWilhoite <amywilhoite@gmail.com>
2022-04-01 01:47:56 -0400

    testing again

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-      const tagData = await tagData.findAll({
+      const tagData = await Tag.findAll({
       include: [{ model: Product }],
     });
     res.status(200).json(tagData);
@@ -50,11 +50,21 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.create(req.body);
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+    const tagData = await Category.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (!categoryData) {
+        res.status(404).json({ message: 'No tag found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 router.delete('/:id', async (req, res) => {
--- README.md
+++ README.md
@@ -24,3 +24,12 @@ Requires terminal and node packages.
 *Link to Repository: 
 
 ## Preview Screens
+Insomnia preview of GET routes to return all categories, all products, and all tags being tested in Insomnia:
+![In Insomnia, the user tests GET categories.](./Assets/get_categories.png)
+![In Insomnia, the user tests GET products.](./Assets/get_products.png)
+![In Insomnia, the user tests GET tags.](./Assets/get_tags.png) 
+
+Insomnia preview of GET routes to one category, product and tag:
+![In Insomnia, the user tests GET categories from 1.](./Assets/get_categories1.png)
+![In Insomnia, the user tests GET products from 1.](./Assets/get_products1.png)
+![In Insomnia, the user tests GET tags from 1](./Assets/get_tags1.png)
